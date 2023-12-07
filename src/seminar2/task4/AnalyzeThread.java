1465d129870b67c5664d7c0d6d41f4cd8d20b233
Tobias <tobias.vinblad0013@stud.hkr.se>
2023-12-07 22:00:57 +0100

    ready to analyze

---
+++ src/seminar2/task4/AnalyzeThread.java
@@ -0,0 +1,73 @@
+package seminar2.task4;
+
+public class AnalyzeThread implements Runnable{
+    private int mode;
+    private int m;
+    private int n;
+    private int times;
+    private double avgTime = 0;
+
+    public AnalyzeThread(int mode, int m, int n, int times) {
+        this.mode = mode;
+        this.m = m;
+        this.n = n;
+        this.times = times;
+    }
+
+    public double getAvgTime() {
+        return avgTime;
+    }
+
+    @Override
+    public void run() {
+        analyze(mode, n, m, times);
+    }
+
+    /**
+     * @param mode 0 = List, 1 = ListIterator, <br>
+     *             2 = LinkedList, 3 = LinkedListIterator
+     */
+    private void analyze(int mode, int n, int m, int times){
+        double tmpSum;
+
+        for (int i = 0; i < times; i++) {
+            long startTime = System.nanoTime();
+
+            runAlgo(mode, n, m);
+
+            long endTime = System.nanoTime();
+            avgTime += (double) (endTime - startTime) / 1000000;  //divide by 1000000 to get milliseconds
+        }
+        avgTime =  avgTime / times;
+    }
+
+    /**
+     * @param mode 0 = List, 1 = ListIterator, <br>
+     *             2 = LinkedList, 3 = LinkedListIterator
+     */
+    private void runAlgo(int mode, int n, int m){
+        switch (mode){
+            case 0:
+                JosephusList josephusList = new JosephusList(n, m);
+                josephusList.runGame();
+                break;
+            case 1:
+                JosephusListIterator josephusListIterator = new JosephusListIterator(n, m);
+                josephusListIterator.runGame();
+                break;
+            case 2:
+                JosephusLinkedList josephusLinkedList = new JosephusLinkedList(n, m);
+                josephusLinkedList.runGame();
+                break;
+            case 3:
+                JosephusLinkedListIterator josephusLinkedListIterator = new JosephusLinkedListIterator(n, m);
+                josephusLinkedListIterator.runGame();
+                break;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(avgTime);
+    }
+}
--- src/seminar2/task4/TestRunningTime.java
+++ src/seminar2/task4/TestRunningTime.java
@@ -6,78 +6,90 @@ public class TestRunningTime {
 
     public static void main(String[] args) {
         automatedAnalazys();
+        automatedAnalazys2();
+
     }
     
     private static void automatedAnalazys(){
-        int n = 100;
-        int m;
-        double[][] duration = new double[4][10];
+        int inputSize = 100_000;
+        int n;
+        int m = 1000;
+        AnalyzeThread[][] duration = new AnalyzeThread[4][10];
+        Thread[][] threads = new Thread[4][10];
         String[] names = {"arrayList", "arrayList with iterator",
                 "own LinkedList", "LinkedList with iterator"};
-        Random rnd = new Random();
+
 
         for (int i = 0; i < duration[0].length; i++) {
-            m = rnd.nextInt(0, n);
-            System.out.println("i now = " + i);
-
+            n = inputSize * (i + 1);
             for (int j = 0; j < duration.length; j++) {
-                System.out.println("j now = " + j);
-                duration[j][i] = analyze(j, n, m, 10);
+                duration[j][i] = new AnalyzeThread(j, m, n, 10);
+
+                threads[j][i] = new Thread(duration[j][i]);
+                threads[j][i].start();
+            }
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            for (int j = 0; j < threads[0].length; j++) {
+                try {
+                    threads[i][j].join();
+                }
+                catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        
+        System.out.printf("%25s |%25s |%25s |%25s |%25s |%25s |%25s |%25s |%25s |%25s |%25s %n", "Algorithm used",
+                inputSize * 1, inputSize * 2, inputSize * 3, inputSize * 4, inputSize * 5,
+                inputSize * 6, inputSize * 7, inputSize * 8, inputSize * 9, inputSize * 10);
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        for (int i = 0; i < duration.length; i++) {
+            System.out.printf("%25s |%25.3f |%25.3f |%25.3f |%25.3f |%25.3f |%25.3f |%25.3f |%25.3f |%25.3f |%25.3f %n", names[i],
+                    duration[i][0].getAvgTime(), duration[i][1].getAvgTime(), duration[i][2].getAvgTime(), duration[i][3].getAvgTime(), duration[i][4].getAvgTime(),
+                    duration[i][5].getAvgTime(), duration[i][6].getAvgTime(), duration[i][7].getAvgTime(), duration[i][8].getAvgTime(), duration[i][9].getAvgTime());
+        }
+
+    }
+
+    private static void automatedAnalazys2(){
+        int n = 100;
+        int m = 1000;
+        AnalyzeThread[][] duration = new AnalyzeThread[4][5];
+        Thread[][] threads = new Thread[4][5];
+        String[] names = {"arrayList", "arrayList with iterator",
+                "own LinkedList", "LinkedList with iterator"};
+
+
+        for (int i = 0; i < duration[0].length; i++) {
+            for (int j = 0; j < duration.length; j++) {
+                duration[j][i] = new AnalyzeThread(j, m, n, 10);
+
+                threads[j][i] = new Thread(duration[j][i]);
+                threads[j][i].start();
             }
             n = n * 10;
         }
 
+        for (int i = 0; i < threads.length; i++) {
+            for (int j = 0; j < threads[0].length; j++) {
+                try {
+                    threads[i][j].join();
+                }
+                catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
         System.out.printf("%25s |%25s |%25s |%25s |%25s |%25s %n", "Algorithm used",
-                "100", "200", "300", "400", "500",
-                "600", "700", "800", "900", "1 000");
-        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                "100", "1 000", "10 000", "100 000", "1 000 000");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
         for (int i = 0; i < duration.length; i++) {
-            System.out.printf("%25s |%25s |%25s |%25s |%25s |%25s |%25s |%25s |%25s |%25s |%25s %n", names[i],
-                    duration[i][0], duration[i][1], duration[i][2], duration[i][3], duration[i][4],
-                    duration[i][5], duration[i][6], duration[i][7], duration[i][8], duration[i][9]);
+            System.out.printf("%25s |%25.3f |%25.3f |%25.3f |%25.3f |%25.3f %n", names[i],
+                    duration[i][0].getAvgTime(), duration[i][1].getAvgTime(), duration[i][2].getAvgTime(), duration[i][3].getAvgTime(), duration[i][4].getAvgTime());
         }
+
     }
-
-    /**
-     * @param mode 0 = List, 1 = ListIterator, <br>
-     *             2 = LinkedList, 3 = LinkedListIterator
-     */
-    private static double analyze(int mode, int n, int m, int times){
-        double sum = 0;
-        for (int i = 0; i < times; i++) {
-            long startTime = System.nanoTime();
-
-            runAlgo(mode, n, m);
-
-            long endTime = System.nanoTime();
-            sum += (double) (endTime - startTime) / 1000000;  //divide by 1000000 to get milliseconds
-        }
-        return sum / times;
-    }
-
-    /**
-     * @param mode 0 = List, 1 = ListIterator, <br>
-     *             2 = LinkedList, 3 = LinkedListIterator
-     */
-    private static void runAlgo(int mode, int n, int m){
-        switch (mode){
-            case 0:
-                JosephusList josephusList = new JosephusList(n, m);
-                josephusList.runGame();
-                break;
-            case 1:
-                JosephusListIterator josephusListIterator = new JosephusListIterator(n, m);
-                josephusListIterator.runGame();
-                break;
-            case 2:
-                JosephusLinkedList josephusLinkedList = new JosephusLinkedList(n, m);
-                josephusLinkedList.runGame();
-                break;
-            case 3:
-                JosephusLinkedListIterator josephusLinkedListIterator = new JosephusLinkedListIterator(n, m);
-                josephusLinkedListIterator.runGame();
-                break;
-        }
-    }
-
 }
